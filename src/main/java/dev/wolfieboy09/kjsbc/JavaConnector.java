ee7db186e59b8e4e2bd5a786abede5fb476b019a
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-03-17 14:50:06 -0400

    And generate to the gen folder

--- src/main/java/dev/wolfieboy09/kjsbc/JavaConnector.java
+++ src/main/java/dev/wolfieboy09/kjsbc/JavaConnector.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 public class JavaConnector {
     public void processCode(String code) {
         System.out.println(code);
-        try (FileWriter writer = new FileWriter("output.js")) {
+        try (FileWriter writer = new FileWriter("generated/output.js")) {
             writer.write(code);
             System.out.println("JavaScript code written to output.js");
         } catch (IOException e) {
