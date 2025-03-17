33fa679e5a5faf0de235e557ac114207efea015f
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-03-17 15:23:22 -0400

    Fix dir creation and writing to it

--- src/main/java/dev/wolfieboy09/kjsbc/JavaConnector.java
+++ src/main/java/dev/wolfieboy09/kjsbc/JavaConnector.java
@@ -1,11 +1,23 @@
 package dev.wolfieboy09.kjsbc;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class JavaConnector {
     public void processCode(String code) {
+        Path folder = Path.of("generated");
         System.out.println(code);
+        if (Files.notExists(folder)) {
+            try {
+                Files.createDirectory(folder);
+            } catch (Exception error) {
+                System.err.println("ERROR - Failed to create directory 'generated'. Error: " + error);
+                return;
+            }
+        }
         try (FileWriter writer = new FileWriter("generated/output.js")) {
             writer.write(code);
             System.out.println("JavaScript code written to output.js");
