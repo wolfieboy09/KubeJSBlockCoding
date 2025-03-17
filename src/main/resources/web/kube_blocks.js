dc997eef86c1bb9d2de3f70a4bfaacc5ee9e4259
wolfieboy09 <105564186+wolfieboy09@users.noreply.github.com>
2025-03-17 14:44:28 -0400

    I can't believe that wrapping the entire thing in a try/catch works great

--- src/main/resources/web/kube_blocks.js
+++ src/main/resources/web/kube_blocks.js
@@ -1,62 +1,68 @@
-let workspace;
+try{
+    let workspace;
 
-function initBlockly() {
-   workspace = Blockly.inject('blocklyDiv', {toolbox: document.getElementById('toolbox')});
-   Blockly.JavaScript.init(workspace)
-}
+    function initBlockly() {
+       workspace = Blockly.inject('blocklyDiv', {toolbox: document.getElementById('toolbox')});
+       Blockly.JavaScript.init(workspace)
+    }
 
-const server_event = {
-  init: function() {
-    this.appendDummyInput('NAME')
-      .appendField('On Server Event')
-      .appendField(new Blockly.FieldDropdown([
-          ['Recipe', 'RECIPE'],
-          ['Test', 'TEST']
-        ]), 'EVENT_TYPE');
-    this.appendStatementInput('statement');
-    this.setTooltip('Listen onto a server event');
-    this.setHelpUrl('');
-    this.setColour(240);
-  }
-};
+    const server_event = {
+      init: function() {
+        this.appendEndRowInput('NAME')
+          .appendField('On Server Event')
+          .appendField(new Blockly.FieldDropdown([
+              ['Recipe', 'recipe'],
+              ['Test2', 'scream'],
+              ['thing', 'thing']
+            ]), 'EVENT_TYPE');
+        this.appendStatementInput('statement');
+        this.setTooltip('Listen onto a server event');
+        this.setHelpUrl('');
+        this.setColour(240);
+      }
+    };
 
-const console = {
-  init: function() {
-    this.appendValueInput('String')
-      .appendField('Console Log');
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setTooltip('');
-    this.setHelpUrl('');
-    this.setColour(225);
-  }
-};
+    const console = {
+      init: function() {
+        this.appendValueInput('toSay')
+          .appendField('Console Log');
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setTooltip('');
+        this.setHelpUrl('');
+        this.setColour(225);
+      }
+    };
 
-Blockly.common.defineBlocks({server_event: server_event});
-Blockly.common.defineBlocks({console: console});
+    Blockly.common.defineBlocks({console: console});
+    Blockly.common.defineBlocks({server_event: server_event});
 
-javascript.javascriptGenerator.forBlock['console'] = function(block, generator) {
-  const value_string = generator.valueToCode(block, 'String', javascript.Order.ATOMIC);
+    javascript.javascriptGenerator.forBlock['console'] = function(block, generator) {
+      // TODO: change Order.ATOMIC to the correct operator precedence strength
+      const value = generator.valueToCode(block, 'toSay', javascript.Order.ATOMIC);
 
-  return `console.log('${value_string}')\n`;
-}
+      const code = `console.log(${value})\n`;
+      return code;
+    }
 
-javascript.javascriptGenerator.forBlock['server_event'] = function(block, generator) {
- var dropdown_event_type = block.getFieldValue('EVENT_TYPE');
- var statements = generator.statementToCode(block, 'statement');
+    javascript.javascriptGenerator.forBlock['server_event'] = function(block, generator) {
+      const dropdown_event_type = block.getFieldValue('EVENT_TYPE');
+      const statements = generator.statementToCode(block, 'statement');
 
- var code = `ServerEvents.${dropdown_event_type}(event => {\n${statements}});\n`;
-  return code
-};
+     var code = `ServerEvents.${dropdown_event_type}(event => {\n${statements}});\n`;
+      return code
+    };
 
-function generateCode() {
-    console.log("Running generation")
-  try {
-      var code = javascript.javascriptGenerator.workspaceToCode(workspace);
-      console.info(code)
-      javaConnector.processCode(code)
-  } catch (error) {
-      console.error("Error in workspaceToCode:", error);
-  }
-  console.log("Generation done")
+    function generateCode() {
+        console.log("Running generation")
+      try {
+          var code = javascript.javascriptGenerator.workspaceToCode(workspace);
+          javaConnector.processCode(code)
+      } catch (error) {
+          console.error("Error in workspaceToCode:", error);
+      }
+      console.log("Generation done")
+    }
+} catch (error) {
+    console.error("Global Error: ", error)
 }
